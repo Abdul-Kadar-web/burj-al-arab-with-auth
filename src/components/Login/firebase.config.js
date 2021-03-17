4f581d4672d5b17b73a7c7c734daccd417ab92b7
Abdul-Kadar-web <emonkhan016844@gmail.com>
2021-03-17 16:06:10 +0600

    burj al arab with auth

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCr9i12IaB4boRnv3E6ZAw0hZuw3aoMsC0",
+    authDomain: "burj-al-arab-ak.firebaseapp.com",
+    projectId: "burj-al-arab-ak",
+    storageBucket: "burj-al-arab-ak.appspot.com",
+    messagingSenderId: "1064177954363",
+    appId: "1:1064177954363:web:f4051fd86706f3d25ef61e"
+  };
+
+  export default firebaseConfig
